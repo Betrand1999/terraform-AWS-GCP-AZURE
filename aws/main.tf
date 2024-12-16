06c45e800aa3ef1b323e2464907588b86193a85c
betrand1999 <132092468+Betrand1999@users.noreply.github.com>
2024-12-16 15:52:54 -0500

    deploy vm to aws (#8)

--- aws/main.tf
+++ aws/main.tf
@@ -2,50 +2,21 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.78.0"
+      version = "5.81.0"
     }
   }
 }
 
 provider "aws" {
+  # Configuration options
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "betrandterraformstatebucketttt"
-    region         = "us-east-1"
-    key            = "terraform_tfstate_file_new"
-    dynamodb_table = "dynamodb_table"
-  }
-}
-
-
-locals {
-  vm_names = ["DEV", "QA"]
-}
-
-resource "aws_instance" "vm_a" {
+resource "aws_instance" "vm_1" {
   ami           = "ami-0453ec754f44f9a4a"
-  instance_type = "t3.micro"
-  count         = 1
+  instance_type = "t2.micro"
   tags = {
-    Name = local.vm_names[count.index]
+    Name = "Dev"
   }
 
 }
-
-
-
-
-
-
-output "name" {
-  value = {
-    IP  = aws_instance.vm_a["0"].public_ip
-    ARN = aws_instance.vm_a["0"].arn
-    AMI = aws_instance.vm_a["0"].ami
-  }
-}
-
-######
